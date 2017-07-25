249d0900d6b7969f396ff38e3f55dc4837c616f6
Cordlesswire <chungwoo007@gmail.com>
2017-07-25 11:38:32 +0200

    Commit

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.tourguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -9,7 +10,7 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextName;
-    Button buttonAdd;
+    Button buttonAddArtist;
     Spinner spinnerGenres;
 
     @Override
@@ -18,9 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editTextName = (EditText) findViewById(R.id.editTextName);
-        buttonAdd = (Button) findViewById(R.id.buttonAddArtist);
+        buttonAddArtist = (Button) findViewById(R.id.buttonAddArtist);
         spinnerGenres = (Spinner) findViewById(R.id.spinnerGenres);
+
+
+        //Add Artist to Firebase when button is clicked
+        buttonAddArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //calling the method addArtist()
+                //the method is defined below
+                //this method is actually performing the write operation
+                // addArtist();
+            }
+        });
+
 
 
     }
 }
+
