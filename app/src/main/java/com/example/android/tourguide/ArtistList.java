bcaa12d5b1e7eff4a5ef7423dd2f1dc6a53a6044
Cordlesswire <chungwoo007@gmail.com>
2017-07-25 12:22:19 +0200

    Commit

---
+++ app/src/main/java/com/example/android/tourguide/ArtistList.java
@@ -0,0 +1,15 @@
+package com.example.android.tourguide;
+
+
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class ArtistList extends ArrayAdapter<Artist>{
+    private Activity context;
+    List<Artist> artistList;
+
+
+
+}
