68a25066721f6ec0ac9c211e74218d6c5a25e942
Cordlesswire <chungwoo007@gmail.com>
2017-07-25 11:25:20 +0200

    Commit

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.android.tourguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    Button buttonAdd;
+    Spinner spinnerGenres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
